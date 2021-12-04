d88245a7d396bf522a452661cc6343376aadddef
calvi7 <jcalvi@fi.uba.ar>
2021-12-04 11:54:43 -0300

    agregue cosas a la app de abm

--- lib/abm.py
+++ lib/abm.py
@@ -2,11 +2,12 @@ from os import path
 
 
 class MainApp:
-    local_count = 0
-    pedidos = path.join(path.dirname(__file__), 'src\\pedidos.csv')
+    pedidos: str = path.join(path.dirname(__file__), 'src\\pedidos.csv')
 
     def __init__(self):
-        self.route = MainApp.pedidos
+        self.route: str = self.pedidos
+        with open(self.route, 'r') as f:
+            self.info_pedidos = f.read().splitlines()
 
     def cargar(self, linea: str):
         """Sirve para cargar pedidos al archivo pedidos.csv agregandolos al final del mismo.  
@@ -21,7 +22,31 @@ class MainApp:
     def verificar_formato(self):
         """Simplemente dice como se deberia cargar cada pedido.
         """
-        with open(self.route, 'r') as f:
-            formato = f.read().splitlines()
+        print(self.info_pedidos[0])
 
-        print(formato[0])
+    def ver_pedidos(self):
+        """Ver los pedidos cargados hasta el momento.
+        """
+        print(self.info_pedidos[0])
+        for i, pedido in enumerate(self.info_pedidos[1:]):
+            print(f'{str(i+1).zfill(2)} - {pedido}')
+
+    def borrar(self):
+        """Borra un pedido del archivo pedidos.csv
+        """
+        print("NICE")
+        self.ver_pedidos()
+        print("Que pedido desea borrar?")
+        try:
+            rta = int(input(""))
+        except ValueError:
+            print("Ingrese un valor numerico.")
+        else:
+            if 0 > rta > len(self.info_pedidos) - 1:
+                print("El valor no esta incluido")
+                # TODO no se tiene que romper cuando se ingresa un valor no valido
+            else:
+                self.info_pedidos.pop(rta-1)
+                with open(self.route, 'w') as f:
+                    for line in self.info_pedidos:
+                        f.writelines(line + '\n')
