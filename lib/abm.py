7920489bb3d8d3e2bf74813d1b6acd1189bac2d3
Dir Ing e Infr - Calvi Juan Marcos <calvi@ea.mil.ar>
2021-12-13 11:14:41 -0300

    Mejor organizacion del abm

--- lib/abm.py
+++ lib/abm.py
@@ -1,65 +1,164 @@
-import csv
+from os import path
+from time import strptime
+from csv import DictReader
+
+PATH: str = "src\\pedidos.csv"
+ROUTE = path.join(path.dirname(__file__), PATH)
 
 
-class MainApp:
-
-    # constructor
-    def __init__(self, __path: str) -> None:
-        """Constructor de la clase MainApp
-
-        Args:
-            __path str: la direccion al archivo pedidos.csv
-        """
-        # el archivo con los pedidos
-        self.route: str = __path
-        # los pedidos en una lista
-        with open(self.route, 'r') as f:
-            self.info_pedidos: list[str] = f.read().splitlines()
-        self.titulos = self.info_pedidos[0].replace(', ', ',')
-        self.pedidos_totales = self.info_pedidos[1:]
+class Pedidos:
+    """El listado de pedidos del archivo csv. Valida que los valores
+    sean ingresados correctamente.
+    """
+    listado: list[str] = []
+    
+    def __init__(self, listado: list[str]) -> None:
+        listado: list[str] = [x.split(",") for x in listado]
         
-        self.actualizar_pedidos()
-
-
-    # metodos de visualizacion de datos
-    def verificar_formato(self, linea) -> bool:
-        """Valida que el formato de un pedido ingresado
+        for i, p in enumerate(listado):
+            
+            fecha, codigo = p[1], p[5]
+            cantidad, descuento = p[7], p[8]
+            
+            self.listado.append(",".join(p))
+            if not self.__validar_fecha(fecha):
+                print(f"El pedido {i+1} tiene errores de formato en la fecha.")
+            if not self.__validar_codigo(codigo):
+                print(f"El pedido {i+1} tiene un codigo de producto invalido.")
+            if not self.__validar_numerico(cantidad, descuento):
+                print(f"El pedido {i+1} tiene valores no numericos ingresados donde deberian"
+                      " ser numericos.")
+            
+    def __validar_fecha(self, fecha: str) -> bool:
+        """Valida que la fecha ingresada sea valida para evitar errores en comparaciones y otras funciones.
 
         Args:
-            linea (str): el pedido que se va a validar
-            
+            fecha (str)
+
         Returns:
-            bool: True si el formato es valido, False si no
+            bool
         """
-        linea = linea.split(',')
-        col1 = isinstance (linea[0], int)
-        col2 = True # TODO
-        col6 = isinstance (linea[5], int)
-        col8 = isinstance (linea[7], int)
-        col9 = isinstance (linea[8], int)
-        return col1 and col2 and col6 and col8 and col9
+        try:
+            strptime(fecha, "%d/%m/%Y")
+        except ValueError:
+            return False
+        else:
+            d, m, y = fecha.split("/")
+            return len(d) == 2 and len(m) == 2 and len(y) == 4
+    
+    def __validar_numerico(self, *args) -> bool:
+        """Valida que los valores ingresados sean numericos.
+        """
+        for arg in args:
+            try:
+                int(arg)
+            except ValueError:
+                return False
+            
+        return True
+    
+    def __validar_codigo(self, codigo: str) -> bool:
+        """Prueba si el codigo sea un int y sea igual a 1334 o 568
+
+        Args:
+            codigo (str): el codigo de producto
+
+        Returns:
+            bool: si es un codigo valido o no
+        """
+        return codigo in ["1334", "568"] if self.__validar_numerico(codigo) else False
+
+
+class Titulos(str):
+    """Los titulos del csv. Verifica que tenga el largo adecuado.
+    """ 
+    def __init__(self, titulos: str) -> None:
+        TOTAL_TITULOS: int = 9
+        titulos: str = titulos.replace(', ', ',')
+        if len(titulos.split(',')) == TOTAL_TITULOS:
+            self.titulos = titulos
+        else:
+            print("La cantidad de titulos no es la adecuada.")    
+
+    def __str__(self):
+        return ', '.join((self.titulos).split(','))
+
+
+class Controlador:
+    """El encargado principal del ABM
+    """
+
+    def __init__(self, route) -> None:
+        self.route = route
+
+        with open(self.route, 'r') as f:
+            self.raw_pedidos: list[str] = f.read().splitlines()
+
+        self.titulos = Titulos(self.raw_pedidos[0])
+        self.pedidos = Pedidos(self.raw_pedidos[1:])
 
     def ver_pedidos(self) -> None:
-        """Ver los pedidos cargados hasta el momento con un formato mas legible
-        """
-        print(self.titulos)
-        for i, pedido in enumerate(self.pedidos_totales):
+        print(self.titulos.titulos)
+        for i, pedido in enumerate(self.pedidos.listado):
             print(f'{str(i+1).zfill(3)} - {pedido}')
-            
+
     def dict_data(self) -> list[dict[any]]:
         """Devuelve la informacion del archivo pedidos.csv en una lista de diccionarios, parecido a un json
 
         Returns:
             list[dict[any]]: 
         """
+        
         lista = []
+        
         with open(self.route, 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
+            reader = DictReader(csvfile)
             for row in reader:
                 l = {x: row[x] for x in self.titulos.split(',')}
                 lista.append(l)
 
         return lista
+
+    def __actualizar(self) -> None:
+        """Actualiza el archivo csv
+        """
+        with open(self.route, 'w') as f:
+            f.writelines(self.titulos.titulos + '\n')
+            for line in self.pedidos.listado:
+                f.writelines(line + '\n')
+    
+    def cargar(self, pedido: str) -> None:
+        """Sirve para cargar pedidos al archivo pedidos.csv agregandolos al final del mismo.  
+
+        Args:
+            linea (str): el pedido a cargar. El formato deberia ser de acuerdo a la primera linea del archivo .csv
+            propuesto en el metodo verificar_formato().
+        """
+        self.pedidos.listado.append(pedido)
+        self.__actualizar()
+                                        
+    def borrar(self, index: int) -> None:
+        """Borra el pedido pasado como parametro del listado
+
+        Args:
+            index (int): indice del pedido que se quiere borrar
+        """
+        self.pedidos.listado.pop(index)
+        self.__actualizar()
+        
+    def modificar(self, index: int, key: str, valor: str) -> None:
+        """Modifica un atributo dado de un pedido dado.
+
+        Args:
+            index (int): El indice en la lista del pedido dado. (numero de pedido - 1)
+            key (str): El atributo que se quiere cambiar del pedido.
+            valor (str): El nuevo valor que se le va a asignar al atributo.
+        """
+        valores_dict = self.dict_data()
+        valores_dict[index][key] = valor
+        
+        self.pedidos.listado[index] = ','.join(valores_dict[index].values())
+        self.__actualizar()
 
     def __date_conversor(self, date: str) -> int:
         """Convierte una fecha de tipo dd/mm/yyyy a yyyymmdd para comparar mas facilmente
@@ -85,62 +184,55 @@ class MainApp:
         listado: list[dict] = self.dict_data()
         return sorted(listado, key=lambda x: self.__date_conversor(x['Fecha']), reverse=not ascendiente)
 
-    def ordenado(self) -> None:
-        lista: list[dict] = self.ordenar_por_antiguedad()
-        print(self.titulos)
-        for i, x in enumerate(lista):
-            print(f"{str(i+1).zfill(2)} - {', '.join(list(x.values()))}")
 
-    # metodos de abm
-    def actualizar_pedidos(self):
-        with open(self.route, 'w') as f:
-            f.writelines(self.titulos + '\n')
-            for line in self.pedidos_totales:
-                f.writelines(line + '\n')
-                
-    def cargar_por_parametros(self, linea: str) -> None:
-        """Sirve para cargar pedidos al archivo pedidos.csv agregandolos al final del mismo.  
+class User:
+    def __init__(self, route) -> None:
+        """Inicializa la clase con la ruta al archivo csv
 
         Args:
-            linea (str): el pedido a cargar. El formato deberia ser de acuerdo a la primera linea del archivo .csv
-            propuesto en el metodo verificar_formato().
+            route ([type]): Ruta al archvio csv
         """
-        with open(self.route, "a") as f:
-            f.writelines(linea + '\n')
+        # abm
+        self.ctrl: Controlador = Controlador(route)
 
+        # titulos
+        self.titulos = self.ctrl.titulos.titulos
+        
     def cargar(self) -> None:
         """Cargar un pedido al archivo .csv
         """
+        print("A continuacion se le pedira que ingrese los siguientes valores: ")
+        print(self.titulos)
+        
         req: dict[any] = {}
         titulos = self.titulos.split(',')
-        add = ""
+        
         for titulo in titulos:
+            add = ""
             if titulo == 'Fecha':
                 add = ' (dd/mm/yyyy)'
             req[titulo] = input(f"{titulo}{add}: ")
-            add = ""
-        self.pedidos_totales.append(','.join(list(req.values())))
-        self.actualizar_pedidos()
-    
+        
+        self.ctrl.cargar(','.join(list(req.values())))
+
     def borrar(self) -> None:
         """Borra un pedido del archivo pedidos.csv
         """
-        self.ver_pedidos()
+        self.ctrl.ver_pedidos()
         print("Que pedido desea borrar?")
         try:
             rta = int(input(""))
         except ValueError:
             print("Ingrese un valor numerico.")
         else:
-            if 1 <= rta <= len(self.pedidos_totales)+1:
-                self.pedidos_totales.pop(rta-1)
-                self.actualizar_pedidos()
+            if 1 <= rta <= len(self.ctrl.pedidos.listado)+1:
+                self.ctrl.borrar(rta-1)
                 print(f"Se borró el pedido {rta}!")
             else:
                 print("El valor no esta incluido")
-
+                
     def modificar(self):  # TODO
-        self.ver_pedidos()
+        self.ctrl.ver_pedidos()
         titulos = self.titulos.split(',')
         try:
             print("Que numero de pedido desea modificar?")
@@ -148,65 +240,34 @@ class MainApp:
         except ValueError:
             print('Ingrese un valor numerico.')
         else:
-            if 1 <= rta <= len(self.pedidos_totales):
-                titulos = self.titulos.split(',')
-                dict_view: list[dict[any]] = self.dict_data()
-                
+            if 1 <= rta <= len(self.ctrl.pedidos.listado):
+
                 print("Se pueden modificar los siguientes atributos del pedido:")
                 print(", ".join(x for x in titulos))
                 print("Cual desea cambiar?")
                 atr: str = input("").title()
                 if atr in titulos:
                     nuevo = input(f"Ingrese el nuevo valor de {atr}: ")
-                    dict_view[rta-1][atr] = nuevo
-                    self.pedidos_totales[rta-1] = ','.join(list(dict_view[rta-1].values()))
-                    self.actualizar_pedidos()
+                    self.ctrl.modificar(rta-1, atr, nuevo)
                 else:
                     print("Ese atributo no existe o esta mal escrito.")
             else:
                 print("Ese pedido no existe.")
-
     
-    # menu
-    def menu(self) -> bool:
-        """Genera un menu en el se incluyen funcionalidades de ABM para el archivo .csv
-        El loop NO esta incluido, pero devuelve un booleano para saber si continua o no
-
-        Returns:
-            bool: Define si sigue el loop o no
+    def ordenado(self) -> None:
+        """Imprime la lista ordenada por antiguedad
         """
-        opciones = {
-            1: 'cargar',
-            2: 'borrar',
-            3: 'modificar pedido',
-            4: 'ver pedidos',
-            5: 'ver pedidos ordenados por antiguedad',
-        }
-        # se agrega la opcion de salir afuera para que siempre sea la ultima
-        # asi se pueden agregar mas opciones antes sin tener que cambiar todo
-        opciones[len(opciones) + 1] = 'salir'
-        print("Opciones:")
-        for key, value in opciones.items():
-            print(f"{key}. {value.capitalize()}")
-        try:
-            rta = int(input(""))
-        except ValueError:
-            print("El valor ingresado debe ser numerico.")
-        else:
-            if rta in list(opciones.keys()):
-                if rta == 1:
-                    self.cargar()
-                elif rta == 2:
-                    self.borrar()
-                elif rta == 3:
-                    self.modificar()
-                elif rta == 4:
-                    self.ver_pedidos()
-                elif rta == 5:
-                    self.ordenado()
-                elif opciones[rta] == 'salir':
-                    print('Adios!')
-                    return False
-                return True
-            else:
-                print("Esa opcion no esta incluida.")
+        print("Quiere ver la lista")
+        lista: list[dict] = self.ctrl.ordenar_por_antiguedad()
+        print(self.titulos)
+        for i, x in enumerate(lista):
+            print(f"{str(i+1).zfill(2)} - {', '.join(list(x.values()))}")
+
+    def dict_data(self) -> list[dict[any]]:
+        return self.ctrl.dict_data()
+
+
+class MainApp(User):
+    def __init__(self, route) -> None:
+        super().__init__(route)
+    
